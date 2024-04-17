76d7e59d0d6eed6616a94639ac693d20bbdf628d
ruoyanj01 <jiang.ruoy@northeastern.edu>
2024-04-17 16:37:23 -0400

    exercise01-05

---
+++ src/exercise01/HelloWorld.java
@@ -0,0 +1,20 @@
+package exercise01;
+
+/*下面是一个简单的Java程序，它将输出“Hello, World!”到控制台。
+这是学习Java的经典入门程序。
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+/*这个程序做了以下几件事情：
+定义了一个 main 方法。main 方法是Java程序的入口点，当你运行一个Java程序时，main 方法会被首先执行。
+在 main 方法中，我们使用 System.out.println 语句来输出 "Hello, World!" 到控制台。
+要运行这个程序，你需要将代码保存为 exercise01.HelloWorld.java 文件，然后在命令行中使用 javac 命令编译它，
+然后再使用 java 命令运行它。具体操作如下：
+javac exercise01.HelloWorld.java
+java exercise01.HelloWorld
+这将会输出 "Hello, World!" 到控制台。
+*/
